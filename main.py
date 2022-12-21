51b9faabdcd7d0c0c05fc7e7b77babcd1c5e77a8
ZoeJune <47944017+ZoeJune@users.noreply.github.com>
2022-12-21 21:53:38 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,13 +20,6 @@ if random_order == "no":
     password_letters = ""
     for string_letters in rand_letters:
         password_letters += string_letters
-        # password_letters = "" + rand_letters[0] # "" = "" + string_letters
-        # password_letters = "rand_letters[0]"
-        # password_letters = "rand_letters[0]" + rand_letters[1]
-        # password_letters = "rand_letters[0]+rand_letters[1]"
-        # password_letters = "rand_letters[0]+rand_letters[1]" + rand_letters[2]
-        # password_letters = "rand_letters[0]+rand_letters[1]+rand_letters[2]"
-        # .....
     password_symbols = ""
     for string_symbols in rand_symbols:
         password_symbols += string_symbols
@@ -43,75 +36,3 @@ elif random_order == "yes":
     for string_letters in password:
         password_letters += string_letters
     print(f"Your password will be: {password_letters}")
-
-# grouping for loops (for not randomized order) in one for loop
-
-#Eazy Level - Order not randomised:
-#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-#Hard Level - Order of characters randomised:
-#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
-
-
-
-
-
-
-
-
-# Kenryu:
-
-
-
-# import random
-#
-# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
-#            'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
-#            'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
-#
-# print("Welcome to the PyPassword Generator!")
-# nr_letters = int(input("How many letters would you like in your password?\n"))
-# nr_symbols = int(input(f"How many symbols would you like?\n"))
-# nr_numbers = int(input(f"How many numbers would you like?\n"))
-#
-#
-# rand_letters = random.choices(letters, weights=None, cum_weights=None, k=nr_letters)
-# rand_symbols = random.choices(symbols, weights=None, cum_weights=None, k=nr_symbols)
-# rand_numbers = random.choices(numbers, weights=None, cum_weights=None, k=nr_numbers)
-#
-# password_letters = ""
-# for string_letters in rand_letters:
-#     password_letters += string_letters
-#     # password_letters = "" + rand_letters[0] # "" = "" + string_letters
-#     # password_letters = "rand_letters[0]"
-#     # password_letters = "rand_letters[0]" + rand_letters[1]
-#     # password_letters = "rand_letters[0]+rand_letters[1]"
-#     # password_letters = "rand_letters[0]+rand_letters[1]" + rand_letters[2]
-#     # password_letters = "rand_letters[0]+rand_letters[1]+rand_letters[2]"
-#     # .....
-#
-# password_symbols = ""
-# for string_symbols in rand_symbols:
-#     password_symbols += string_symbols
-#
-# password_numbers = ""
-# for string_numbers in rand_numbers:
-#     password_numbers += string_numbers  # advanced: making loop out of all
-#
-# #print(f"Your password will be: {password_letters}{password_symbols}{password_numbers}")
-#
-#
-# # characters in randomized order
-#
-# rand_letters.extend(rand_symbols)
-# rand_letters.extend(rand_numbers)
-#
-# password = random.sample(rand_letters, k=len(rand_letters))
-#
-# password_letters = ""
-# for string_letters in password:
-#     password_letters += string_letters
-#
-# print(f"Your password will be: {password_letters}")
-
